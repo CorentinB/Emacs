3b02b0fb6f130ed760dc038acdf6decf0dac0c82
gregory.voloir <gregory.voloir@epitech.eu>
2018-02-08 11:34:45 +0100

    install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+safe_mkdir () {
+    if [ ! -d $1 ]; then
+	mkdir -p $1
+    fi
+}
+
+cd
+safe_mkdir old_emacs
+safe_mkdir new_emacs
+
+cd new_emacs
+git clone https://github.com/Kaserta/Emacs_conf.git
+cd
+cp -r .emacs.d old_emacs
+cp -r .emacs old_emacs
+
+cp -r new_emacs/.emacs.d ./.emacs.d
+cp -r new_emacs/.emacs ./.emacs
